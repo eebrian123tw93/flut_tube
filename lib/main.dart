20586969135a46cb8ba63bebe946fc54a9c9799e
Brian93 <eebrian123tw93@gmail.com>
2024-02-21 21:24:04 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:rive_splash_screen/rive_splash_screen.dart';
 import 'splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
