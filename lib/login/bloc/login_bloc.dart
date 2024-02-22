8b0092b029441b4267e44b7079cad93c83cdb615
Brian93 <eebrian123tw93@gmail.com>
2024-02-22 22:54:26 +0800

    update

--- lib/login/bloc/login_bloc.dart
+++ lib/login/bloc/login_bloc.dart
@@ -19,10 +19,14 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   LoginBloc(this._userRepository) : super(LoginState.empty()) {
     on<EmailChanged>((event, emit) {
       state.update(isEmailValid: Validators.isValidEmail(event.email));
-    }, transformer: droppable());
+    }, transformer: (events, mapper) {
+      return events.debounceTime(const Duration(milliseconds: 300)).asyncExpand(mapper);
+    });
 
     on<PasswordChanged>((event, emit) {
       state.update(isPasswordValid: Validators.isValidPassword(event.password));
+    }, transformer: (events, mapper) {
+      return events.debounceTime(const Duration(milliseconds: 300)).asyncExpand(mapper);
     });
 
     on<LoginWithGooglePressed>((event, emit) async {
